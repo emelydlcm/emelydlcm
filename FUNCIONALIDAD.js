49b22b16f82045fd4f2d4c3eaf2d2e2031e69922
emelydlcm <delacruzemely07@gmail.com>
2025-03-09 14:53:34 -0600

    Add files via upload

---
+++ FUNCIONALIDAD.js
@@ -0,0 +1,5 @@
+// Función para manejar el envío del formulario
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por ponerte en contacto! Te responderemos pronto.');
+});
